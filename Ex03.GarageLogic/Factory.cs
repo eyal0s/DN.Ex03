5bd501745d95b66cdf5c70c1685d81921dd20ba7
ItayAb <itayabra1@gmail.com>
2015-05-23 14:21:38 +0300

    testing

--- Ex03.GarageLogic/Factory.cs
+++ Ex03.GarageLogic/Factory.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 
 // put the init logic here. now "new" outside this class
+
+
 namespace Ex03.GarageLogic
 {
     class Factory
     {
+        Console.WriteLine("Eyal Is GAY!!!!!!!!!!!!!!!!!!");
+        Console.WriteLine("The Gayest!!!!!");
     }
 }
