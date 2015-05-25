776968ea9936475eff87f120e94775137b46fee5
ItayAb <itayabra1@gmail.com>
2015-05-25 18:34:10 +0300

    bla

--- Ex03.GarageLogic/Car.cs
+++ Ex03.GarageLogic/Car.cs
@@ -4,31 +4,35 @@ using System.Text;
 
 namespace Ex03.GarageLogic
 {
-	class Car : Vehicle
-	{
-		private const float k_MaxAirPressureCar = 31;
-		private const int k_NumberOfWheels = 4;
-		private string m_ColorOfCar;
-		private int m_AmountOfDoors;
-		 //electric characteristics
-		private const float k_MaxBatteryTime = 2.2f;
-		//petrol characteristics
-		private const float k_MaxTankLiter = 35f;
-        private const Petrol.eFuelType k_FuelType = Petrol.eFuelType.Octan96;
+    class Car : Vehicle
+    {
+        private eColor m_ColorOfCar;
+        private int m_AmountOfDoors;
 
-		public Car(string i_Manufacturer, string i_LicenseNumber, string i_WheelManufacturer, float i_CurrentAvailableHours, string i_ColorOfCar, int i_NumOfDoors, FuelSource i_FuelSource) :
-			base(i_Manufacturer, i_LicenseNumber, k_NumberOfWheels, k_MaxAirPressureCar, i_WheelManufacturer , i_FuelSource)
-		{         
-			m_ColorOfCar = i_ColorOfCar;
-			m_AmountOfDoors = i_NumOfDoors;		
-		}
+        public Car(string i_Manufacturer, string i_LicenseNumber, string i_WheelManufacturer, float i_CurrentAvailableHours, int i_NumberOfWheels, float i_MaxAirPressure, eColor i_ColorOfCar, int i_NumOfDoors, FuelSource i_FuelSource) :
+            base(i_Manufacturer, i_LicenseNumber, i_NumberOfWheels, i_MaxAirPressure, i_WheelManufacturer, i_FuelSource)
+        {
+            if (i_NumOfDoors > 5 || i_NumOfDoors < 2)
+            {
+                throw new ValueOutOfRangeException("Car can have between 2 and 5 doors");
+            }
+
+            m_ColorOfCar = i_ColorOfCar;
+            m_AmountOfDoors = i_NumOfDoors;
+        }
 
 
-		public override string ToString()
-		{
-			return string.Format("{0}, Number of doors: {1}, Color of car: {2}", base.ToString() , m_AmountOfDoors, m_ColorOfCar);
-		}
+        public override string ToString()
+        {
+            return string.Format("{0}, Number of doors: {1}, Color of car: {2}", base.ToString(), m_AmountOfDoors, m_ColorOfCar);
+        }
 
-
-	}
+        public enum eColor
+        {
+            White,
+            Black,
+            Green,
+            Red
+        }
+    }
 }
