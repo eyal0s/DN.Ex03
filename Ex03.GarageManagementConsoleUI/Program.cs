cad17e7d175e537bdd89b8bfd864f005a53994b9
ItayAb <itayabra1@gmail.com>
2015-05-25 18:32:17 +0300

    revereted

--- Ex03.GarageManagementConsoleUI/Program.cs
+++ Ex03.GarageManagementConsoleUI/Program.cs
@@ -15,47 +15,25 @@ namespace Ex03.GarageManagementConsoleUI
             Console.WriteLine("Option 2");
             Console.WriteLine("Option 3");
             Console.WriteLine();
-            //Console.Write("input: ");
+            Console.Write("input: ");
 
             var originalpos = Console.CursorTop;
 
             var k = Console.ReadKey();
-            var i = 1;
+            var i = 2;
 
             while (k.KeyChar != 'q')
             {
 
                 if (k.Key == ConsoleKey.UpArrow)
                 {
-                    i = (i + 1) % 3;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.BackgroundColor = ConsoleColor.Black;
+
                     Console.SetCursorPosition(0, Console.CursorTop - i);
                     Console.ForegroundColor = ConsoleColor.Black;
                     Console.BackgroundColor = ConsoleColor.White;
                     Console.WriteLine("Option " + (Console.CursorTop + 1));
                     Console.ResetColor();
-                    
-
-                }
-
-                if (k.Key == ConsoleKey.DownArrow)
-                {
-                    i = (i - 1) % 3;
-                    Console.SetCursorPosition(0, Console.CursorTop + i);
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.BackgroundColor = ConsoleColor.White;
-                    //Console.WriteLine("Option " + (Console.CursorTop + 1));
-                    Console.ResetColor();
-                    
-
-                }
-
-                if (k.Key == ConsoleKey.Enter)
-                {
-
-                    Console.WriteLine(i);
-                    Console.Clear();
+                    i++;
 
                 }
 
