a0d14d13897e979b7af1e85532a52375eb0bbbc2
eyal0s <eyalbe@gmail.com>
2015-05-29 16:32:55 +0300

    ui...

--- Ex03.GarageManagementConsoleUI/Program.cs
+++ Ex03.GarageManagementConsoleUI/Program.cs
@@ -10,11 +10,13 @@ namespace Ex03.GarageManagementConsoleUI
 
         public static void Main(string[] args)
         {
+            bool isRunning = true;
 
-            while (true)
+            while (isRunning)
             {
                 Console.Clear();
                 Console.WriteLine("Hello and welcome to our garage. Please pick an action from the list: ");
+
                 Console.WriteLine("Display all the possibilities here...");
 
                 char input = Console.ReadKey().KeyChar;
@@ -22,7 +24,7 @@ namespace Ex03.GarageManagementConsoleUI
 
                 switch (selection)
                 {
-                    case eGarageAction.InsertVeihcle:
+                    case eGarageAction.InsertVehicle:
                         insertVeihcle();
                         break;
                     case eGarageAction.DisplayLicenseList:
@@ -30,7 +32,7 @@ namespace Ex03.GarageManagementConsoleUI
                         displayLicenseList();
                         break;
                     case eGarageAction.ChangeVehicleState:
-                        insertVeihcle();
+                        changeVehicleState();
                         break;
                     case eGarageAction.InflateWheels:
                         inflateWheels();
@@ -45,53 +47,61 @@ namespace Ex03.GarageManagementConsoleUI
                         displayVeihcleInfo();
                         break;
                     case eGarageAction.Quit:
-                        exitGarage();
+                        Console.WriteLine("Bye bye!");
+                        isRunning = false;
                         break;
                     default:
-                        Console.WriteLine(input + " is an invalid option. Options are between 1-7");
+                        Console.WriteLine(input + " is an invalid option. Options are between 1-8");
                         break;
                 } 
             }
         }
 
+        private static void changeVehicleState()
+        {
+            //eyal
+            throw new NotImplementedException();
+        }
+
         private static void displayVeihcleInfo()
         {
+            // itex
             throw new NotImplementedException();
         }
 
         private static void recharge()
         {
+            // eyal
             throw new NotImplementedException();
         }
 
         private static void refuel()
         {
+            // eyal
             throw new NotImplementedException();
         }
 
         private static void inflateWheels()
         {
+            //eyal
             throw new NotImplementedException();
         }
 
         private static void displayLicenseList()
         {
+            // filter by enum of state - eyal
             throw new NotImplementedException();
         }
 
         private static void insertVeihcle()
         {
+            //itex
             throw new NotImplementedException();
-        }
-
-        private static void exitGarage()
-        {
-            Console.WriteLine("Bye bye!");
         }
 
         public enum eGarageAction
         {
-            InsertVeihcle = 1,
+            InsertVehicle = 1,
             DisplayLicenseList = 2,
             ChangeVehicleState = 3,
             InflateWheels = 4,
